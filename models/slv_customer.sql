a1850150d9fd3a8fec302f2f7ed36e15457003b3
Nainsi kumari <nainsirani@gmail.com>
2026-07-12 11:10:13 +0000

    incremental materlized

--- models/slv_customer.sql
+++ models/slv_customer.sql
@@ -8,4 +8,6 @@
 with cte_slv_customer as(
     select * from  {{ref('brz_customer')}} where first_name is not null and email is not null 
 )
-select * from cte_slv_customer
+select customer_id,first_name,last_name,email,signup_date,
+    TO_TIMESTAMP(last_updated, 'MM/DD/YYYY HH24:MI:SS') AS last_updated
+ from cte_slv_customer
